3482d9ea75ba5fa79c336fdcf2720f1ec49baa39
Sicheng He <hschsc@umich.edu>
2022-07-25 18:05:42 -0400

    add derivative

--- src/extra_file.jl
+++ src/extra_file.jl
@@ -1,1 +1,3 @@
 my_f(x, y) = 2x + 3y
+my_f2(x) = 2x
+derivative_of_my_f(x) = ForwardDiff.derivative(my_f2, x)
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,11 @@ using Test
 
 @testset "MyExample.jl" begin
     # Write your tests here.
-    @test MyExample.my_f(2, 1) == 7
-    @test MyExample.my_f(2, 3) == 13
+    @test my_f(2, 1) == 7
+    @test my_f(2, 3) == 13
+end
+
+@testset "Derivative tests" begin
+    # Write your tests here.
+    @test derivative_of_my_f(2) == 2
 end
